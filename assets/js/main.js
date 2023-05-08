f306059ebacc44a9a7c2eb211bee058ec0fbb963
noravers <gia.drago4@gmail.com>
2023-05-08 13:32:21 -0600

    bmi calculation vanilla js

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,5 +43,39 @@ const calculateBmi = (e) => {
     e.preventDefault()
 
     //Check if the fileds have a value
+    if(calculateCm.value === '' || calculateKg.value === ''){
+        calculateMessage.classList.remove('color-green')
+        calculateMessage.classList.add('color-red')
+
+        calculateMessage.textContent = 'Fill in the Height and Weight'
+        setTimeout(e => {
+            calculateMessage.textContent = ''
+        }, 3000)
+    } else {
+        const cm = calculateCm.value/100,
+            kg = calculateKg.value,
+            bmi = (kg/(cm*cm)).toFixed(2);
+        const skinnyStandard = 18.5
+        const normalStandard = 25
+        calculateMessage.classList.add('color-green')
+        if(bmi <  skinnyStandard) {
+            calculateMessage.textContent = `Your BMI is ${bmi} and you are too skinny`
+        } else if (bmi < normalStandard) {
+            calculateMessage.textContent = `Your BMI is ${bmi} and you are healthy`
+        } else if (bmi > normalStandard + 5 ) {
+            calculateMessage.textContent = `Your BMI is ${bmi} and you are overweight`
+        }
+
+        calculateCm.value = ''
+        calculateKg.value = ''
+
+        setTimeout(e => {
+            calculateMessage.textContent = ''
+        }, 4000)
+    }
+
+   
     
 }
+
+calculateForm.addEventListener('submit', calculateBmi)
