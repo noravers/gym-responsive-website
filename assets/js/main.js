0b5a2e289281960b0bb9c7ceb3566aca28450113
noravers <gia.drago4@gmail.com>
2023-05-11 02:39:40 -0600

    scroll active

--- assets/js/main.js
+++ assets/js/main.js
@@ -106,8 +106,36 @@ const sendEmail = (e) => {
                 setTimeout(e => {
                     contactMessage.textContent = ''
                 }, 3000)                
+            }, (error) => {
+                alert('Oops, something went wrong...', error)
             })
     }
+
+    contactUser.value = ''
 }
 
 contactForm.addEventListener('submit', sendEmail)
+
+
+//SCROLL SECTIONS ACTIVE LINK
+
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }else {
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
