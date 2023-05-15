827d6cc203b129b70200571d262c4cf5088aae09
noravers <gia.drago4@gmail.com>
2023-05-15 01:17:08 -0600

    scroll reveal

--- assets/js/main.js
+++ assets/js/main.js
@@ -150,6 +150,24 @@ const scrollUp = () => {
 
 window.addEventListener('scroll', scrollUp)
 
+//SCROLL REVEAL
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+})
+
+// sr.reveal(`.home__data, .footer__group, .footer__container`)
+sr.reveal(`.home__data`)
+sr.reveal(`.home__img`, { delay: 700, origin: 'bottom' })
+sr.reveal(`.logos__img`, { interval: 100 })
+sr.reveal(`.logos__img, .program__card, .pricing__card`, { interval: 100 })
+sr.reveal(`.choose__img, .calculate__content`, { origin: 'left' })
+sr.reveal(`.choose__content, .calculate__img`, { origin: 'right' })
+sr.reveal(``)
+
 
 
 
